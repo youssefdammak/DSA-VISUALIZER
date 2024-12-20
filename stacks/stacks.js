276ff90af21085f1f779d2529aa8555ebd0a5fc7
youssef dammak <youssedbenmaherdammak@gmail.com>
2024-12-20 14:40:30 -0500

    added the stack visulization page

---
+++ stacks/stacks.js
@@ -0,0 +1,28 @@
+const box = document.getElementById("box");
+const pushButton = document.getElementById('pushBtn');
+const popButton = document.getElementById("popBtn");
+let stack=[];
+function pushToStack(){
+    if(stack.length<10)
+    {
+        const bar=document.createElement('div');
+        bar.className="bar";
+        bar.textContent=stack.length+1;
+        box.appendChild(bar);
+        stack.push(stack.length+1);
+    }
+    else{
+        alert("stack overflow");
+    }
+}
+function popFromStack(){
+    if (stack.length>0){
+        box.removeChild(box.lastChild);
+        stack.pop();
+    }
+    else{
+        alert("stack underflow");
+    }
+}
+pushButton.addEventListener('click', () => {pushToStack();});
+popButton.addEventListener('click', () => {popFromStack();});
